3568b744ce73fb9e064c6d40fe8972fd3b853ce4
Yash <yash1402agr@gmail.com>
2023-06-29 22:57:52 +0530

    commit

--- src/Index.js
+++ src/Index.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 import bodyParser from 'body-parser';
 import UserSchema from "../Schema/user.js";
 // import dbUrl from "./config/keys.js"
-import validateLogin from "../Auth/validateLogin.js"
-import validateSignIn from "../Auth/validateSignIn.js"
+import validateLogin from "../Auth/ValidateLogin.js"
+import validateSignIn from "../Auth/ValidateSignIn.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import 'dotenv/config'
